e3232530cef5a7bb4c80016e9679a3465e4c4587
Robert Nițu <niturobert02@yahoo.com>
2023-06-14 18:15:47 +0300

    Task done

--- Section 2/Assignment 1/assignment/src/app/app.module.ts
+++ Section 2/Assignment 1/assignment/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { WarningAlertComponent } from './warning-alert/warning-alert.component';
 import { SuccessAlertComponent } from './success-alert/success-alert.component';
+import { HorizontalLineComponent } from './horizontal-line/horizontal-line.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     WarningAlertComponent,
-    SuccessAlertComponent
+    SuccessAlertComponent,
+    HorizontalLineComponent
   ],
   imports: [
     BrowserModule
---
+++ Section 2/Assignment 1/assignment/src/app/horizontal-line/horizontal-line.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: '.app-horizontal-line',
+  template: '<hr>',
+  styles: [`
+    hr {
+      border: 2px solid cadetblue;
+    }
+  `]
+})
+export class HorizontalLineComponent {
+
+}
