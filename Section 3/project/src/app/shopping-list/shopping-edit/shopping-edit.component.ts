fa2ff380cba4de018e80560b9ed7304bb87d527f
Robert Nițu <niturobert02@yahoo.com>
2023-06-19 15:30:37 +0300

    Done section 6

--- Section 3/project/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ Section 3/project/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ViewChild,
+  ElementRef,
+  EventEmitter,
+  Output,
+} from '@angular/core';
+import { Ingredient } from '../../../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
-  styleUrls: ['./shopping-edit.component.css']
+  styleUrls: ['./shopping-edit.component.css'],
 })
 export class ShoppingEditComponent {
+  @ViewChild('ingredientNameInput') ingredientNameInput: ElementRef;
+  @ViewChild('ingredientAmountInput') ingredientAmountInput: ElementRef;
 
+  @Output('onIngredientAdded') ingredientAdded = new EventEmitter<Ingredient>();
+
+  addIngredient() {
+    // console.log(this.ingredientNameInput.nativeElement.value);
+    // console.log(this.ingredientAmountInput.nativeElement.value);
+    this.ingredientAdded.emit(
+      new Ingredient(
+        this.ingredientNameInput.nativeElement.value,
+        this.ingredientAmountInput.nativeElement.value
+      )
+    );
+  }
 }
