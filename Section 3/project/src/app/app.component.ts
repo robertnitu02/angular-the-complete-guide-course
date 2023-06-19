7cc591ae1f82a4398efadc62c508db3f1c838712
Robert Nițu <niturobert02@yahoo.com>
2023-06-19 15:01:06 +0300

    Modified section 3 project with data binding

--- Section 3/project/src/app/app.component.ts
+++ Section 3/project/src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
 
+export enum NavbarItem {
+  Recipes,
+  ShoppingList,
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'project';
+  protected readonly NavbarItem = NavbarItem;
+
+  currentNavbarItem = NavbarItem.Recipes;
+
+  navbarItemSelected(itemSelected: number) {
+    console.log(itemSelected);
+    this.currentNavbarItem = itemSelected;
+  }
 }
--- Section 3/project/src/app/header/header.component.ts
+++ Section 3/project/src/app/header/header.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {
+  @Output('onNavbarItemSelected') navbarItemSelected =
+    new EventEmitter<number>();
 
+  itemSelected(item: number) {
+    this.navbarItemSelected.emit(item);
+  }
 }
