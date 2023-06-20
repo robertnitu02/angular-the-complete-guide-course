122c73e19d57e38c1e233c5dc2d8ed47892cecca
Robert Nițu <niturobert02@yahoo.com>
2023-06-20 16:06:54 +0300

    Added ngSwitch model. Done section 7

--- Section 7/section-seven/src/app/app.component.ts
+++ Section 7/section-seven/src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'section-seven';
+export class AppComponent implements OnInit {
+  randomNumber: number = 0;
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+    this.randomNumber = Math.floor(Math.random() * 5) + 1;
+  }
 }
