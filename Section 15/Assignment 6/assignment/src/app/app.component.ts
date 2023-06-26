1546f6dc6f354bc61dfbc6ffe26b54a8b83e8285
Robert Nițu <niturobert02@yahoo.com>
2023-06-26 18:24:41 +0300

    Task done

--- Section 15/Assignment 6/assignment/src/app/app.component.ts
+++ Section 15/Assignment 6/assignment/src/app/app.component.ts
@@ -1,4 +1,11 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
+import {NgForm} from '@angular/forms';
+
+export interface Data {
+  email: string;
+  password: string;
+  subscription: string;
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +13,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'assignment';
+  @ViewChild('form') registerForm: NgForm;
+
+  defaultSubscription = 'advanced';
+  submitted = false;
+  registerData: Data = {email: '', password: '', subscription: ''};
+
+  subscriptions = ['Basic', 'Advanced', 'Pro'];
+
+  onSubmit() {
+    console.log(this.registerForm);
+
+    this.registerData.email = this.registerForm.value.email;
+    this.registerData.password = this.registerForm.value.password;
+    this.registerData.subscription = this.registerForm.value.subscription;
+
+    this.submitted = true;
+  }
 }
