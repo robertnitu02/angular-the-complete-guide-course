9791b66d8efc8225e15aef88c524840bf34bdf17
Robert Nițu <niturobert02@yahoo.com>
2023-06-27 17:39:44 +0300

    Task done

--- Section 15/Assignment 7/assignment/src/app/app.component.ts
+++ Section 15/Assignment 7/assignment/src/app/app.component.ts
@@ -1,9 +1,51 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {Observable} from "rxjs";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  invalidProjectName = 'Test';
+
+  projectForm: FormGroup;
+
+  ngOnInit(): void {
+    console.clear();
+    this.projectForm = new FormGroup({
+      'projectName': new FormControl(null, [
+        Validators.required,
+        // this.checkProjectName.bind(this)
+      ], this.checkProjectNameAsync.bind(this)),
+      'email': new FormControl(null, [Validators.required, Validators.email]),
+      'status': new FormControl('stable')
+    });
+  }
+
+  checkProjectName(formControl: FormControl): { [s: string]: boolean } {
+    if (formControl.value === this.invalidProjectName)
+      return {'isProjectNameValid': true};
+    return null;
+  }
+
+  checkProjectNameAsync(formControl: FormControl): Promise<any> | Observable<any> {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (formControl.value === this.invalidProjectName)
+          resolve({'isProjectNameValid': true});
+        else
+          resolve(null);
+      }, 1500);
+    });
+  }
+
+  onSubmit() {
+    console.clear();
+    console.log(this.projectForm);
+    this.projectForm.reset({
+      'status': new FormControl('stable')
+    });
+  }
 }
