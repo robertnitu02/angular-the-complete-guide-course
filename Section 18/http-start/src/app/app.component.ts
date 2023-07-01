28b6368d5e0b51f1586163d6bf8ef47024031ea9
Robert Nițu <niturobert02@yahoo.com>
2023-07-01 14:19:03 +0300

    Working post and get methods

--- Section 18/http-start/src/app/app.component.ts
+++ Section 18/http-start/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import {map} from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -13,18 +14,43 @@ export class AppComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.fetchPosts();
   }
 
   onCreatePost(postData: { title: string; content: string }) {
     // Send Http request
-    console.log(postData);
+    // console.log(postData);
+    this.http.post(
+      'https://angular-course-a0eb0-default-rtdb.firebaseio.com/posts.json',
+      postData
+    ).subscribe((responseData: any) => {
+      console.log(responseData);
+    });
   }
 
   onFetchPosts() {
     // Send Http request
+    this.fetchPosts();
   }
 
   onClearPosts() {
     // Send Http request
+  }
+
+  private fetchPosts() {
+    this.http
+      .get('https://angular-course-a0eb0-default-rtdb.firebaseio.com/posts.json')
+      .pipe(map((responseData: any) => {
+        const responseArray = [];
+        for (const key in responseData) {
+          if (responseData.hasOwnProperty(key)) {
+            responseArray.push({...responseData[key], id: key});
+          }
+        }
+        return responseArray;
+      }))
+      .subscribe((posts: any) => {
+        console.log(posts);
+      });
   }
 }
